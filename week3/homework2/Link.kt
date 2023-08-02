a545d271a63240ba1ce3f26d1d6504ecb17b79e3
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-02 23:49:30 +0300

    Add files via upload

---
+++ week3/homework2/Link.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinakademiodev.homeworks.week3.homework2
+
+/*
+Medium yazısı yazılacak. Medium konusu Android Uygulama Geliştirme Temelleri: Proje Yapısı,
+Kullanıcı Arayüzü Tasarımı ve Resource Yönetimi
+ */
+
+//https://medium.com/@byesude/android-uygulama-gelistirme-temelleri-bdd9783b2943
---
+++ week3/homework5/FirstThread.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinakademiodev.homeworks.week3.homework5
+
+class FirstThread : Thread() {
+    override fun run() {
+        for (i in 100..199) {
+            println("Birinci thread: $i")
+            Thread.sleep(5)
+        }
+    }
+}
