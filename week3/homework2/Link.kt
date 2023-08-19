3d6ceb0943eea4e518b81a24225241ff9413999d
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-19 15:58:02 +0300

    Delete week3 directory

--- week3/homework2/Link.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinakademiodev.homeworks.week3.homework2
-
-/*
-Medium yazısı yazılacak. Medium konusu Android Uygulama Geliştirme Temelleri: Proje Yapısı,
-Kullanıcı Arayüzü Tasarımı ve Resource Yönetimi
- */
-
-//https://medium.com/@byesude/android-uygulama-gelistirme-temelleri-bdd9783b2943
