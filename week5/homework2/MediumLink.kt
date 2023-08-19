75fd5a20ae77d4651e8e464dcf879a7403652854
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-19 19:06:34 +0300

    Add files via upload

---
+++ week5/homework2/MediumLink.kt
@@ -0,0 +1,7 @@
+package com.sisterslab.kotlinacademyhomeworks.week5.homework2
+
+/*
+Medium yazı konusu: Kotlin ile Android Programlama Listeler, RecyclerView ve Adapter Tasarımı
+ */
+
+//https://medium.com/@byesude/list-recyclereview-adapter-37efac541479
