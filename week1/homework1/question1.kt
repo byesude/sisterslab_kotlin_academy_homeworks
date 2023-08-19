ea72f80ab021e118bb2d52637f703e553c111dee
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-19 15:58:58 +0300

    Add files via upload

---
+++ week1/homework1/question1.kt
@@ -0,0 +1,38 @@
+package com.sisterslab.kotlinacademyhomeworks.week1.homework1
+
+/*
+Dışardan kullanıcı adı ve şifre girişi alınacak, girilen bu kullanıcı adı ve şifre önceden belirlenen
+kullanıcı adı ve şifreyle  eşleştirilecek . Kullanıcı adında büyük-küçük harf farkı göz ardı edilecek,
+ancak şifrede büyük-küçük harf farkı olması gerekmektedir. kullanıcı adı ve şifre daha önce belirlenen
+kullanıcı adı ve şifreyle uyuşuyorsa hoş geldiniz diyecek eşleşmiyorsa kullanıcı adı veya şifre hatalı
+uyarı verecek program yazılmalı
+ */
+import java.util.Scanner
+
+fun main() {
+
+    val ad = "Ecem"
+    val sifre = "Ecem2002"
+
+
+    val scanner = Scanner(System.`in`)
+
+    // Kullanıcı adı ve şifre girişini al
+    print("Kullanıcı Adı: ")
+    val kullaniciAdi = scanner.nextLine()
+
+    print("Şifre: ")
+    val kullaniciSifre = scanner.nextLine()
+
+
+    if (kullaniciAdi.equals(ad, ignoreCase = true) && kullaniciSifre.equals(
+            sifre,
+            ignoreCase = false
+        )
+    ) {
+        println("Hoş geldiniz!")
+    } else {
+        println("Kullanıcı adı veya şifre hatalı.")
+    }
+
+}
---
+++ week1/homework6/MediumLink.kt
@@ -0,0 +1,7 @@
+package com.sisterslab.kotlinacademyhomeworks.week1.homework6
+
+/*
+Medium yazı konusu: Kotlin Programlamada Tür Dönüşümü ve Konsol Girdisi: Veri İşleme ve Etkileşim
+ */
+
+//https://medium.com/@byesude/kotlin-programlama-4f58895c9b59
