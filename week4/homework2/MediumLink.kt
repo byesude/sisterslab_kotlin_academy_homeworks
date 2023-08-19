d0260b5897e9965de3632c5cf2b25f52358861e4
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-19 19:04:56 +0300

    Add files via upload

---
+++ week4/homework2/MediumLink.kt
@@ -0,0 +1,7 @@
+package com.sisterslab.kotlinacademyhomeworks.week4.homework2
+
+//Medium Linkleri
+
+//Kalıtım ve Miras = https://medium.com/@byesude/kotlinde-miras-alma-dae70719c03b
+
+//Dizilerde Veri Saklama ve İşlemler = https://medium.com/@byesude/kotlinde-diziler-5d398f413c73
