f52882214b0919b10eee90ece7168b1e14ffbf53
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-19 15:58:12 +0300

    Delete week4 directory

--- week4/homework2/MediumLinkleri.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinakademiodev.homeworks.week4.homework2
-
-//Medium Linkleri
-
-//Kalıtım ve Miras = https://medium.com/@byesude/kotlinde-miras-alma-dae70719c03b
-
-//Dizilerde Veri Saklama ve İşlemler = https://medium.com/@byesude/kotlinde-diziler-5d398f413c73
