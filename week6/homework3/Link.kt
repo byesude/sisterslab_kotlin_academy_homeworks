00de57bf7e30043e22be4f73e32416608b8e0200
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-26 23:53:34 +0300

    Add files via upload

---
+++ week6/homework3/Link.kt
@@ -0,0 +1,7 @@
+package com.sisterslab.kotlinacademyhomeworks.week6.homework3
+
+/*
+Medium yazısı: Navigation Component Kullanımı
+ */
+
+// Link: https://medium.com/@byesude/navigation-component-bfd52a8bf89f
